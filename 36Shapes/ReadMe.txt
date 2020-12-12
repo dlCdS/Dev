f5cfe83b1569dd8b7b6fb6692077ed8e8fccec5e
CdS <francis.denis541@gmail.com>
2020-12-12 20:23:03 +0100

    added 36Shapes wav files

---
+++ 36Shapes/ReadMe.txt
@@ -0,0 +1,5 @@
+Several shapes at 9600 sample precision
+
+One shape file contains several waveform for continus transition from one another
+
+For Serum use, go to wavetable editor, enter 9600 in the formula field and then dragndrop the .wav file. It should be corectly slitted. The recommended morph transition is the crossfade mode, but feel free to use another one.
--- push.bat
+++ push.bat
@@ -4,7 +4,7 @@ call git.bat
 
 call cleanup.bat
 
-%git_bash% -c "git add CdS_Base FunctionDrawer Dogodo CdS_Data/Dogodo/Data iPlug2-master/36Common iPlug2-master/36Effects VstRelease push.bat  pull.bat cleanup.bat"
+%git_bash% -c "git add CdS_Base FunctionDrawer Dogodo CdS_Data/Dogodo/Data iPlug2-master/36Common iPlug2-master/36Effects VstRelease 36Shapes push.bat  pull.bat cleanup.bat"
 
 %git_bash% -c "git commit"
 %git_bash% -c "git push -u origin master"
